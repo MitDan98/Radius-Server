139e6d2e354fa4aa9db019f7f9e31752f741843c
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-10-24 20:03:47 +0300

    Update Radius.sh

--- Radius.sh
+++ Radius.sh
@@ -31,8 +31,7 @@ if [ $? -ne 0 ]; then
 fi
 firewall-cmd --add-service={http,https,radius} --permanent
 firewall-cmd --reload
-firewall-cmd --get-default-zone
-public
+firewall-cmd --get-default --zone=public
 firewall-cmd --list-services --zone=public
 dhcpv6-client http https radius ssh
 pkill radius
